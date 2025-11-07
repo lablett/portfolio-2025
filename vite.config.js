f2330b9e2803d8e30df97312eeba358bce889910
Lucille Ablett <lucilleablett@gmail.com>
2025-11-07 09:56:30 +0000

    Add base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "/portfolio-2025/",
     plugins: [react(), "@tailwindcss/postcss"],
     resolve: {
         alias: {
