887ade5a5cc4e381d4a7e2632354554a2f2b321a
Lucille Ablett <lucilleablett@gmail.com>
2025-11-07 17:33:59 +0000

    Remove base path for custom domain deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/portfolio-2025/",
     plugins: [react(), "@tailwindcss/postcss"],
     resolve: {
         alias: {
