a3feac9a11bece981179a9f6181bc71ab565b44b
Lucille Ablett <lucilleablett@gmail.com>
2025-11-07 09:17:55 +0000

    Fix photo import for build

--- src/Components/About/Photo.jsx
+++ src/Components/About/Photo.jsx
@@ -1,11 +1,12 @@
 import AnimatedImage from '@Components/AnimatedImage'
 import { colors } from '@constants/colors'
 import { animationDelays } from '@constants/animations'
+import greenImage from '@/assets/green.png'
 
 const Photo = ({ isInView }) => {
   return (
     <AnimatedImage
-      src="/src/assets/green.png"
+      src={greenImage}
       alt="Profile"
       borderColor={colors.hotPink}
       isInView={isInView}
