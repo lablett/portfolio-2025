acb6f42d2798ee2475afd3f60eed07aba554d678
Lucille Ablett <lucilleablett@gmail.com>
2025-11-10 12:10:13 +0000

    CV and bio updates

--- src/constants/bio.js
+++ src/constants/bio.js
@@ -1,9 +1,9 @@
 export const bioData = {
   name: 'Lucille Ablett',
   title: 'Software Engineer',
-  description: `I am a full-stack engineer with commercial experience in React, TypeScript, Ruby, Python. I’ve worked across web and mobile, from greenfield feature development to legacy refactors and platform migrations. I care about writing maintainable code, clear communication, and building software that’s a pleasure to use.
+  description: `I am a full-stack engineer with commercial experience in React, TypeScript, Ruby, and Python. I’ve worked across web and mobile, from greenfield feature development to legacy refactors and platform migrations. I care about writing maintainable code, clear communication, and building software that’s a pleasure to use.
   \n\n
   Before tech, I worked in geology and data science. I spent several years at an open-pit copper mine in Zambia, setting up processes from scratch and teaching myself to code macros to automate geological workflows. Later, I completed an MSc in spatial analytics and machine learning, which led to a data science role in the mining industry.  Eventually, I realised I preferred building software to analysing data, so I taught myself web development and moved into full-stack engineering.
   \n\n
-  Outside of work, I organise immersive art spaces at volunteer-run events in the UK and Spain, and spend downtime with experimental baking, paper art, sci-fi, and logic puzzles.`,
+  Outside of work, I organise immersive art spaces at volunteer-run events in the UK and Spain, and spend my downtime with experimental baking, paper art, sci-fi, and logic puzzles.`,
 }
